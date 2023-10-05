a5f03156cb46431a547870d332c6167b368b67cf
Noelshub <noel.gabrielsson@gmail.com>
2023-10-05 18:58:23 +0200

    d

--- Dicegame.py
+++ Dicegame.py
@@ -1,2 +1,30 @@
+import random
+
+
 def maingame():
-    player= 0
+    
+    def kasta_tarningen():
+        return random.randint(1, 6)
+        
+    
+    
+
+    def spelarens_kast():
+        
+
+
+
+
+        
+
+    spelarens_kast()    
+    
+
+
+    
+                 
+
+
+
+
+maingame()
